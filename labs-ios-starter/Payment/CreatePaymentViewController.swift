b6e7846189b6a32aab510b4c7f9b5d06032fa8d2
Lydia Zhang <art4everyan@gmail.com>
2020-08-27 10:13:08 -0400

    cocoa file made and connected to UI

---
+++ labs-ios-starter/Payment/CreatePaymentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePaymentViewController.swift
+//  labs-ios-starter
+//
+//  Created by Lydia Zhang on 8/27/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class CreatePaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
