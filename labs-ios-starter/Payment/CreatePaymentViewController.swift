2543c298701c1151f80ce395998f4e8c7e924a3b
Lydia Zhang <art4everyan@gmail.com>
2020-08-27 10:19:18 -0400

    COCOA file connects to the ui outlets and actions

--- labs-ios-starter/Payment/CreatePaymentViewController.swift
+++ labs-ios-starter/Payment/CreatePaymentViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class CreatePaymentViewController: UIViewController {
 
+    
+    //MARK: -UIOutlets
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var amountTextField: UITextField!
+    
+    @IBOutlet var ACHButton: UIButton!
+    @IBOutlet var creditButton: UIButton!
+    @IBOutlet var debitButton: UIButton!
+    @IBOutlet var wireButton: UIButton!
+    @IBOutlet var otherButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
-
+    //MARK: -PAYMENT BUTTON
+    @IBAction func ACHTapped(_ sender: Any) {
+    }
+    @IBAction func creditTapped(_ sender: Any) {
+    }
+    @IBAction func debitTapped(_ sender: Any) {
+    }
+    @IBAction func wireTapped(_ sender: Any) {
+    }
+    @IBAction func otherTapped(_ sender: Any) {
+    }
+    
+    
+    //MARK: -DONE BUTTON
+    @IBAction func doneTapped(_ sender: Any) {
+    }
+    
 }
