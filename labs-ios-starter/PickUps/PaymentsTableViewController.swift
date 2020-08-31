90c3aa5cc1663b32129d44bb4f80691f621115d4
Lydia Zhang <art4everyan@gmail.com>
2020-08-31 12:55:02 -0400

    Made table view cells

--- labs-ios-starter/PickUps/PaymentsTableViewController.swift
+++ labs-ios-starter/PickUps/PaymentsTableViewController.swift
@@ -15,7 +15,11 @@ class PaymentsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupViews()
     }
     
     func setupViews() {
@@ -30,22 +34,17 @@ class PaymentsTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return payments.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentCell", for: indexPath)
+        
+        let payment = payments[indexPath.row]
+        cell.detailTextLabel?.text = payment.dueDate
+        cell.textLabel?.text = payment.hospitalityContractid
         return cell
     }
     
